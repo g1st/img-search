94225656767e3eef982749d25a5645471084910c
g1st <gintstan@gmail.com>
2017-09-28 19:49:20 +0100

    finished search functionallity

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const mongodb = require('mongodb').MongoClient;
 const GoogleImages = require('google-images');
-
-const img_key = process.env.IMG_KEY || 'AIzaSyAKELwNHTYoh572KXd1DLIWn-dW-1VuBso';
-const cse_id = process.env.CSE_ID || '017784079179170889714:reot0j32gsm'
-const PORT = process.env.PORT || 3000;
-
-const imgClient = new GoogleImages(cse_id, img_key);
-
+require('dotenv').config();
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+const imgClient = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
 
 app.use(express.static(__dirname + '/'));
 
@@ -16,8 +13,11 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/api', (req, res) => {
-  imgClient.search('funny cat')
+app.get('/api/imagesearch/:search_term', (req, res) => {
+  // if offset is anything but number - show the first page
+  const page = isNaN(req.query.offset) ? 1 : req.query.offset;
+
+  imgClient.search(req.params.search_term, {page: page})
       .then(images => {
         images.map(image => {
           delete image.type;
@@ -26,30 +26,11 @@ app.get('/api', (req, res) => {
           delete image.width;
           image.thumbnail = image.thumbnail.url;
         });
-        // console.log(images);
-          // [{
-          //     "url": "http://steveangello.com/boss.jpg",
-          //     "type": "image/jpeg",
-          //     "width": 1024,
-          //     "height": 768,
-          //     "size": 102451,
-          //     "thumbnail": {
-          //         "url": "http://steveangello.com/thumbnail.jpg",
-          //         "width": 512,
-          //         "height": 512
-          //     }
-          // }];
           res.send(images);
       })
       .catch((err) => {
         console.error(err);
       });
 });
-
-// paginate results
-// imgClient.search('Steve Angello', {page: 2});
-
-// search for certain size
-// imgClient.search('Steve Angello', {size: 'large'});
 
 app.listen(PORT, () => `app is listening on port ${PORT}`);
